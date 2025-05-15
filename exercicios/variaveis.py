c1441e98cfe15c8c96e7ef9e1488c4549ffcfa26
Rodrigo <rodrigoserafimfsilva@gmail.com>
2025-05-15 19:56:03 -0300

    Nova atualização

---
+++ exercicios/variaveis.py
@@ -0,0 +1,12 @@
+nome = "Rodrigo"
+#_nome = "Rodrigo"
+#ano = 2025
+sobrenome_rodrigo = "Serafim"
+Nome = "SerafimR"
+
+idade = 30
+altura = 1.8
+
+print(nome)
+print(type(idade))
+print(type(altura))
