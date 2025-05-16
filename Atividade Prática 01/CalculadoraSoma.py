0c11b261cad164745b1a663756993d23620261bd
Rodrigo <rodrigoserafimfsilva@gmail.com>
2025-05-16 20:01:41 -0300

    Primeira Atividade EDN

---
+++ Atividade Prática 01/CalculadoraSoma.py
@@ -0,0 +1,6 @@
+# Calculadora de Soma
+numero1 = 12
+numero2 = 14
+soma = numero1 + numero2
+
+print("A soma é:", soma)
---
+++ Atividade Prática 01/CalculadoraVolume.py
@@ -0,0 +1,8 @@
+# Calculadora de Volume de uma caixa retangular
+comprimento = 12  # em cm
+largura = 14      # em cm
+altura = 20       # em cm
+
+volume = comprimento * largura * altura
+
+print("O volume da caixa é:", volume, "cm³")
